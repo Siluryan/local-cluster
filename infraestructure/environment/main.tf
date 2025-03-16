b6486fb0e02f22e24bc4b3d96951b1670ffcf447
Guilherme Dias <guilhermerrdias@gmail.com>
2025-03-16 15:29:35 -0300

    :sparkles: feat: criacao inicial da infraestrutura

--- infraestructure/environment/main.tf
+++ infraestructure/environment/main.tf
@@ -5,7 +5,7 @@ module "helm" {
     client_secret = var.oauth2_proxy_client_secret
     cookie_secret = var.oauth2_proxy_cookie_secret
 
-    cloudflare_email = var.cloudflare_email
-    cloudflare_tunnel_id = var.cloudflare_tunnel_id
+    cloudflare_email        = var.cloudflare_email
+    cloudflare_tunnel_id    = var.cloudflare_tunnel_id
     cloudflare_tunnel_token = var.cloudflare_tunnel_token
 }
